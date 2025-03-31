a18048bacccefa269e245bba3cba61c34c84e02e
Gabriel <gdmusse@hotmail.com>
2025-03-31 12:09:58 -0300

    commit teste

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -20,7 +20,7 @@
         public decimal Dividir(long a, long b)
         {
             if (b == 0)
-                throw new DivideByZeroException("Não é possível dividir por zero.");
+                throw new DivideByZeroException("Não é possível dividir por zero");
             return (decimal)a / b;
         }
     }
